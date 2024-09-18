067bc7c1f4fd5a55c3b578abab8f32a56f2fde4d
DESKTOP-V7TK7FL\admin <1187493252@qq.com>
2024-09-18 15:55:22 +0800

    no message

---
+++ UnityFramework/Runtime/Resource/ResourceComponent.cs
@@ -0,0 +1,22 @@
+﻿/*
+* FileName:          ResourceComponent
+* CompanyName:       
+* Author:            relly
+* Description:       
+*/
+
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityFramework.Runtime
+{
+    [DisallowMultipleComponent]
+    public class ResourceComponent
+    {
+
+    }
+}
